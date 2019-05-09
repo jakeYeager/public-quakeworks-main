550dfbde0b9b8e8e7bce96ccc01d6838ba0cde61
JYeager <jake@yeager.works>
2019-05-08 20:30:06 -0700

    fix 404

--- project/.vuepress/config.js
+++ project/.vuepress/config.js
@@ -14,10 +14,10 @@ module.exports = {
     head: [
         ['link', { rel: 'icon', href: `/hero-logo.png` }],
         ['meta', { name: 'theme-color', content: '#3366cc' }],
-        ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: './icons/apple-touch-icon.png' }],
-        ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: './icons/favicon-32x32.png' }],
-        ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: './icons/favicon-16x16.png' }],
-        ['link', { rel: 'manifest', href: './icons/site.webmanifest' }]
+        ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/icons/apple-touch-icon.png' }],
+        ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/icons/favicon-32x32.png' }],
+        ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/icons/favicon-16x16.png' }],
+        ['link', { rel: 'manifest', href: '/icons/site.webmanifest' }]
     ],
     title: 'Quake Works', // default page/site title
     description: 'Quake works is a data-centric research project by Jake Yeager, which focuses on creating fault system status and indicator models, rather than chasing triggers and predictions.', // default page meta tag
